09b430dd78f01f3797f151311f2c09628ea5d8bd
Nathan Alden <visualcsharpcoder@hotmail.com>
2012-11-02 19:38:39 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Timers;
+
+namespace NathanAlden.FirefoxAutoRefresh
+{
+	internal static class Program
+	{
+		private static readonly HashSet<string> _changedPaths = new HashSet<string>();
+		private static readonly string _directory = ConfigurationManager.AppSettings["Directory"];
+		private static readonly string[] _filters = ConfigurationManager.AppSettings["Filters"].Split(';');
+		private static readonly string _host = ConfigurationManager.AppSettings["Host"];
+		private static readonly object _lockObject = new object();
+		private static readonly ushort _port = UInt16.Parse(ConfigurationManager.AppSettings["Port"]);
+		private static readonly Timer _timer = new Timer
+			{
+				AutoReset = false
+			};
+		private static readonly HashSet<FileSystemWatcher> _watchers = new HashSet<FileSystemWatcher>();
+		private static TelnetClient _client;
+		private static TimeSpan _refreshDelay = TimeSpan.FromMilliseconds(Int32.Parse(ConfigurationManager.AppSettings["RefreshDelayInMilliseconds"]));
+
+		private static void Main()
+		{
+			ConfigureTimer();
+			AddWatchers();
+
+			Console.WriteLine("Press ESC to exit");
+			Console.WriteLine();
+			Console.WriteLine("Monitoring changes in {0}", _directory);
+			Console.WriteLine("Filters: {0}", String.Join(" ", _filters));
+			Console.WriteLine();
+			Console.WriteLine("Connecting to {0} port {1}...", _host, _port);
+
+			_client = new TelnetClient(_host, _port, Connected, () => Ready(_watchers));
+			_client.Connect();
+
+			while (true)
+			{
+				ConsoleKeyInfo key = Console.ReadKey();
+
+				if (key.Key != ConsoleKey.Escape)
+				{
+					continue;
+				}
+
+				break;
+			}
+
+			_client.Disconnect();
+
+			foreach (FileSystemWatcher watcher in _watchers)
+			{
+				watcher.EnableRaisingEvents = false;
+			}
+		}
+
+		private static void ConfigureTimer()
+		{
+			_timer.Interval = _refreshDelay.TotalMilliseconds;
+			_timer.Elapsed += TimerOnElapsed;
+		}
+
+		private static void AddWatchers()
+		{
+			foreach (string filter in _filters)
+			{
+				var watcher = new FileSystemWatcher(_directory, filter)
+					{
+						IncludeSubdirectories = true
+					};
+
+				watcher.Changed += WatcherOnChanged;
+				watcher.Created += WatcherOnChanged;
+				watcher.Deleted += WatcherOnChanged;
+				watcher.Renamed += WatcherOnChanged;
+
+				_watchers.Add(watcher);
+			}
+		}
+
+		private static void Connected()
+		{
+			Console.WriteLine("Connected, waiting for reply...");
+		}
+
+		private static void Ready(IEnumerable<FileSystemWatcher> watchers)
+		{
+			Console.WriteLine("Firefox is ready to accept commands");
+
+			foreach (FileSystemWatcher watcher in watchers)
+			{
+				watcher.EnableRaisingEvents = true;
+			}
+		}
+
+		private static void WatcherOnChanged(object sender, FileSystemEventArgs e)
+		{
+			_timer.Stop();
+			_timer.Start();
+
+			lock (_lockObject)
+			{
+				_changedPaths.Add(e.FullPath);
+			}
+		}
+
+		private static void TimerOnElapsed(object sender, ElapsedEventArgs e)
+		{
+			lock (_lockObject)
+			{
+				Console.WriteLine();
+				foreach (string changedPath in _changedPaths)
+				{
+					Console.WriteLine(changedPath);
+				}
+				_client.WriteBrowserReload();
+				Console.WriteLine("Browser refreshed");
+			}
+		}
+	}
+}
---
+++ TelnetClient.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NathanAlden.FirefoxAutoRefresh
+{
+	public class TelnetClient
+	{
+		private readonly Action _connectedDelegate;
+		private readonly StringBuilder _data = new StringBuilder();
+		private readonly string _host;
+		private readonly ushort _port;
+		private readonly Action _readyDelegate;
+		private TcpClient _client;
+
+		public TelnetClient(string host, ushort port, Action connectedDelegate, Action readyDelegate)
+		{
+			if (host == null)
+			{
+				throw new ArgumentNullException("host");
+			}
+			if (connectedDelegate == null)
+			{
+				throw new ArgumentNullException("connectedDelegate");
+			}
+			if (readyDelegate == null)
+			{
+				throw new ArgumentNullException("readyDelegate");
+			}
+
+			_host = host;
+			_port = port;
+			_connectedDelegate = connectedDelegate;
+			_readyDelegate = readyDelegate;
+		}
+
+		public string Host
+		{
+			get
+			{
+				return _host;
+			}
+		}
+
+		public ushort Port
+		{
+			get
+			{
+				return _port;
+			}
+		}
+
+		public bool Ready
+		{
+			get;
+			private set;
+		}
+
+		public void Connect()
+		{
+			Disconnect();
+
+			_client = new TcpClient(_host, _port);
+
+			_connectedDelegate();
+
+			NetworkStream stream = _client.GetStream();
+			var buffer = new byte[_client.ReceiveBufferSize];
+
+			stream.BeginRead(buffer, 0, buffer.Length, ReadCallback, buffer);
+		}
+
+		public void WriteBrowserReload()
+		{
+			NetworkStream stream = _client.GetStream();
+			byte[] buffer = Encoding.ASCII.GetBytes(";BrowserReload();");
+
+			stream.BeginWrite(buffer, 0, buffer.Length, WriteCallback, null);
+		}
+
+		public void Disconnect()
+		{
+			if (_client == null)
+			{
+				return;
+			}
+
+			_client.Close();
+			_client = null;
+			_data.Clear();
+			Ready = false;
+		}
+
+		private void ReadCallback(IAsyncResult ar)
+		{
+			NetworkStream stream = _client.GetStream();
+			var buffer = (byte[])ar.AsyncState;
+			int bytesRead = stream.EndRead(ar);
+
+			if (bytesRead == 0)
+			{
+				return;
+			}
+
+			string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+			_data.Append(data);
+
+			if (_data.ToString().EndsWith("repl> "))
+			{
+				Ready = true;
+				_readyDelegate();
+			}
+			else
+			{
+				stream.BeginRead(buffer, 0, buffer.Length, ReadCallback, buffer);
+			}
+		}
+
+		private void WriteCallback(IAsyncResult ar)
+		{
+			NetworkStream stream = _client.GetStream();
+
+			stream.EndWrite(ar);
+		}
+	}
+}
